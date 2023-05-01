ef8e4c4d6e972414f80d010f0b109d79e8abb51e
Christian Vazquez Hope <christian@digithings.no>
2023-05-01 12:39:08 +0200

    Build with js and css

--- js/blog.js
+++ js/blog.js
@@ -1,7 +1,69 @@
-const api = "hhttps://dev.digithings.io/wp-api/wp-json/wp/v2/posts";
+const api = "https://dev.digithings.io/wp-api/wp-json/wp/v2/posts";
 const params = new URLSearchParams({
-    categories: "17",
+  categories: "17",
+  per_page: 10,
+  _embed: true
 });
 
-const urlParams = new URLSearchParams(window.location.search);
-const productId = urlParams.get('slug');
+const postContainer = document.querySelector(".container.blog");
+const loadingIndicator = document.createElement("div");
+loadingIndicator.classList.add("loading");
+loadingIndicator.textContent = "Loading posts...";
+postContainer.appendChild(loadingIndicator);
+
+function renderPosts(posts) {
+  postContainer.innerHTML = '';
+  
+  posts.forEach(post => {
+    const postWrapper = document.createElement("div");
+    postWrapper.classList.add("item");
+
+    const postLink = document.createElement("a");
+    postLink.classList.add("link");
+    postLink.href = `/blog/?id=${post.id}`;
+
+    const postImage = document.createElement("img");
+    postImage.classList.add("image");
+    postImage.src = post._embedded["wp:featuredmedia"][0].source_url;
+    postImage.alt = post.title.rendered;
+    postLink.appendChild(postImage);
+
+    const postTitle = document.createElement("h2");
+    postTitle.classList.add("title");
+    postTitle.textContent = post.title.rendered;
+    postLink.appendChild(postTitle);
+
+    postWrapper.appendChild(postLink);
+    postContainer.appendChild(postWrapper);
+  });
+
+  const moreButton = document.createElement("button");
+  moreButton.classList.add("more-btn");
+  moreButton.textContent = "Load more posts";
+  moreButton.addEventListener("click", () => {
+    params.set('page', parseInt(params.get('page')) + 1);
+    fetchPosts(params);
+  });
+  postContainer.appendChild(moreButton);
+}
+
+function fetchPosts(params) {
+  fetch(`${api}?${params.toString()}`)
+    .then(response => {
+      if (response.ok) {
+        return response.json();
+      } else {
+        throw new Error("Sorry, there was an error loading the posts... Please try refreshing the page.");
+      }
+    })
+    .then(posts => {
+      renderPosts(posts);
+    })
+    .catch(error => {
+      const errorMessage = document.createElement("div");
+      errorMessage.textContent = "Sorry, there was an error loading the posts... Please try refreshing the page.";
+      postContainer.appendChild(errorMessage);
+    });
+}
+
+fetchPosts(params);
